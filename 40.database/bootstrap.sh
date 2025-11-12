8d894a28c26658c16ae9d25c708cf72e8740d9cd
DINESH6677 <dgadila@my.bridgeport.edu>
2025-11-12 08:26:27 -0500

    terraform

--- 40.database/bootstrap.sh
+++ 40.database/bootstrap.sh
@@ -2,4 +2,24 @@
 
 component=$1
 dnf install ansible -y
-ansible-pull -U https://github.com/DINESH6677/ansible-roles-tf.git -e component=$component main.yaml
+# ansible-pull -U https://github.com/DINESH6677/ansible-roles-tf.git -e component=$component main.yaml
+
+REPO_URL=https://github.com/DINESH6677/ansible-roles-tf.git
+REPO_DIR=/opt/roboshop/ansible
+ANSIBLE_DIR=ansible-roles-tf
+
+mkdir  -p $REPO_DIR
+mkdir -p /var/log/roboshop
+touch ansible.log
+
+cd $REPO_DIR
+
+if [ -d $ANSIBLE_DIR];then
+    cd ANSIBLE_DIR
+    git pull
+else
+    git clone $REPO_URL
+    cd $ANSIBLE_DIR
+fi
+
+ansible-playbook -e component=$component main.yaml 
