f17e8fc054e4fd67d9d3c8c69dcc8f20541d8f14
DINESH6677 <dgadila@my.bridgeport.edu>
2025-11-14 16:58:26 -0500

    terraform

---
+++ 60.catalogue/data.tf
@@ -0,0 +1,32 @@
+data "aws_ami" "instance" {
+  most_recent = true
+  owners = ["973714476881"]
+
+  filter {
+    name   = "name"
+    values = ["RHEL-9-DevOps-Practice"]
+  }
+
+  filter {
+    name   = "platform-details"
+    values = ["Red Hat Enterprise Linux"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+}
+
+data "aws_ssm_parameter" "private_subnet_ids" {
+  name = "/${var.project_name}/${var.environment}/private_subnet_ids"
+}
+
+data "aws_ssm_parameter" "catalogue_sg_id" {
+  name = "/${var.project_name}/${var.environment}/catalogue_sg_id"
+}
