fb8bf7ca68a77de4952a47863f94e8fda60e8437
DINESH6677 <dgadila@my.bridgeport.edu>
2025-11-15 19:08:26 -0500

    terraform

--- 50.backend_alb/variables.tf
+++ 50.backend_alb/variables.tf
@@ -5,3 +5,11 @@ variable "project_name" {
 variable "environment" {
     default = "dev"
 }
+
+variable "zone_id" {
+    default = "Z09655201PB8BBQ9OOULW"
+}
+
+variable "domain_name" {
+    default = "devopswithdinesh.shop"
+}
